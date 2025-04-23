2a8405a5d1d29602c34b400a0a0f9a10b083415d
victoralmeida428git config --global user.password 95546353Vg <victor.almeida@unigranrio.br>
2025-04-23 11:11:47 -0300

    update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,28 @@
-import type { NextConfig } from "next";
+// next.config.ts
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    async headers() {
+        const isProduction = process.env.NEXT_PUBLIC_VERCEL_ENV === 'production';
+
+        return [
+            {
+                source: '/:path*',
+                headers: [
+                    {
+                        key: 'X-Robots-Tag',
+                        value: isProduction ? 'index, follow' : 'noindex, nofollow',
+                    },
+                    {
+                        key: 'Cache-Control',
+                        value: isProduction
+                            ? 'public, max-age=3600, stale-while-revalidate=86400'
+                            : 'no-cache',
+                    },
+                ],
+            },
+        ];
+    },
 };
 
 export default nextConfig;
